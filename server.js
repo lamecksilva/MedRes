0f93ea57aa1aecfda1247bec07acb5ed46e762b9
lameck <lameck2@hotmail.com>
2018-12-05 12:07:59 -0300

    Adicionando express

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("First App");
+});
+
+app.listen(3000, () => {
+  console.log("Rodando na porta 3000");
+});
